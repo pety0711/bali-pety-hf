03dbb50fdde83373c6de0e41cd95e27c29d2415f
sbali90 <sbali888@yahoo.com>
2016-11-07 19:23:12 +0100

    Added BL Project

---
+++ src/CoffeBook/BL/DbHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB;
+using DB.Entities;
+
+namespace BL
+{
+    public class DbHelper
+    {
+        public static void TestDb()
+        {
+            using (var context = new CoffeBookContext())
+            {
+                var conn = context.Database.Connection.ConnectionString;
+                context.Database.Delete();
+                context.Database.CreateIfNotExists();
+                var user = new User
+                {
+                    Name = "Feri",
+                    Password = "teve"
+                };
+                var coffee = new Coffee { Name = "Kofi" };
+                var recipe = new Recipe
+                {
+                    Name = "Kofi Recipe",
+                    CoffeeType = coffee,
+                    Description = "Put in stuff you like and drink."
+                };
+                var recipeBook = new RecipeBook
+                {
+                    Name = "Good Recipes",
+                    Recipes = new List<Recipe> { recipe }
+                };
+                context.Recipes.Add(recipe);
+                context.RecipeBooks.Add(recipeBook);
+                context.Users.Add(user);
+                context.Coffes.Add(coffee);
+                context.SaveChanges();
+            }
+            using (var db = new CoffeBookContext())
+            {
+                var coffees = db.Coffes.ToList();
+                var users = db.Users.ToList();
+                var recipes = db.Recipes.ToList();
+                var recipeBooks = db.RecipeBooks.ToList();
+                var userRecipeBooks = (from user in db.Users
+                                       where user.Name.Equals("Feri")
+                                       select user.RecipeBooks).ToList();
+                var recipesInBook = (from recipeBook in db.RecipeBooks
+                                     where recipeBook.Name.Equals("Good Recipes")
+                                     select recipeBook.Recipes).ToList();
+            }
+        }
+    }
+}
