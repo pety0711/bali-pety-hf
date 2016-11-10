f5d5fef5b5eb3d7c86b87a0299188cc0c0e7615b
pety0711 <pety.science@gmail.com>
2016-11-10 13:53:52 +0100

    UI development in progress

--- src/CoffeBook/CoffeBook/App.xaml.cs
+++ src/CoffeBook/CoffeBook/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace CoffeBook
 {
@@ -13,5 +15,19 @@ namespace CoffeBook
     /// </summary>
     public partial class App : Application
     {
+        public static Frame CurrentFrame { get; set; }
+
+        public static NavigationService Navigation;
+        
+
+        public static bool Navigate(Type sourcePage, object parameter)
+        {
+            return CurrentFrame.Navigate(sourcePage, parameter);
+        }
+
+        public static void GoBack()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ src/CoffeBook/CoffeBook/Commands/CommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CoffeBook.Commands
+{
+    class CommandHandler : ICommand
+    {
+        private Action action;
+        private bool canExecute;
+        public CommandHandler(Action action, bool canExecute)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
